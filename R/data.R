781e959e3bb979be267402ecdb475c527042b8d2
samuelbfernandes <samuelfernandes@agronomo.eng.br>
2019-08-01 15:06:59 -0500

    Example Data Set

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Genotypic data for 282 inbred association panel using the 55K SNP array. 
+#' Reference: Cook et al. 2012. Genetic architecture of maize kernel 
+#' composition in the nested association mapping and inbred association panels. 
+#' Plant Physiology 158:824-834
+#'
+#' @format A data frame with 51286 rows and 285 variables:
+#' @source \url{10.1104/pp.111.185033}
+"SNP55K_maize282"
