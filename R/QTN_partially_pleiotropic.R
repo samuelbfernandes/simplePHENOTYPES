ce23cb25b5cbb85b8d5f214ebc0672425bd1ab2a
samuelbfernandes <samuelfernandes@agronomo.eng.br>
2019-09-23 13:31:57 -0500

    new QTN_partially_pleiotropic

--- R/QTN_partially_pleiotropic.R
+++ R/QTN_partially_pleiotropic.R
@@ -3,13 +3,12 @@
 #' @param genotypes = NULL,
 #' @param seed = NULL,
 #' @param overlap = NULL,
-#' @param overlapE = NULL,
-#' @param specific.QTN.number = NULL,
-#' @param specific.E.QTN.number = NULL,
+#' @param overlap_e = NULL,
+#' @param specific_QTN_number = NULL,
+#' @param specific_e_QTN_number = NULL,
 #' @param ntraits = NULL
 #' @return Genotype of selected SNPs
 #' @author Alex lipka and Samuel Fernandes
-
 #' Last update: Jul 22, 2019
 #'
 #'----------------------------- QTN_partially_pleiotropic ----------------------
@@ -17,29 +16,27 @@ QTN_partially_pleiotropic <-
   function(genotypes = NULL,
            seed = NULL,
            overlap = NULL,
-           overlapE = NULL,
-           specific.QTN.number = NULL,
-           specific.E.QTN.number = NULL,
+           overlap_e = NULL,
+           specific_QTN_number = NULL,
+           specific_e_QTN_number = NULL,
            ntraits = NULL) {
     #---------------------------------------------------------------------------
-    # Randomly select (without replacement) k additive QTN, and 
+    # Randomly select (without replacement) k additive QTN, and
     # assign an effect size using data.table::fwrite from data.table package
     if (!is.null(seed)) {
       set.seed(seed)
     }
     # First SNP at column 6
-    vector.of.pleiotropic.add.QTN <-
+    vector_of_pleiotropic_add_QTN <-
       sample(6:nrow(genotypes), overlap, replace = FALSE)
-    
-    Add.pleiotropic.QTN.genotypic.info <-
-      genotypes[vector.of.pleiotropic.add.QTN, ]
-    
+    add_pleiotropic_QTN_genotypic_info <-
+      genotypes[vector_of_pleiotropic_add_QTN, ]
     data.table::fwrite(
-      Add.pleiotropic.QTN.genotypic.info,
+      add_pleiotropic_QTN_genotypic_info,
       paste0(
-        "Genotypic.information.for.",
+        "Genotypic_information_for_",
         overlap,
-        ".pleiotropic.Additive.QTN",
+        "_pleiotropic_Additive_QTN",
         ".txt"
       ),
       row.names = FALSE,
@@ -47,33 +44,29 @@ QTN_partially_pleiotropic <-
       quote = FALSE,
       na = NA
     )
-    
     snps <-
-      setdiff(6:nrow(genotypes), vector.of.pleiotropic.add.QTN)
-    vector.of.specific.add.QTN <- list()
-    Add.specific.QTN.genotypic.info <- list()
+      setdiff(6:nrow(genotypes), vector_of_pleiotropic_add_QTN)
+    vector_of_specific_add_QTN <- list()
+    add_specific_QTN_genotypic_info <- list()
     ss <- c()
     for (i in 1:ntraits) {
       if (!is.null(seed)) {
         ss[i] <- seed + i
         set.seed(seed + i)
       }
-      vector.of.specific.add.QTN[[i]] <-
-        sample(snps, specific.QTN.number[i], replace = FALSE)
-      
-      snps <- setdiff(snps, vector.of.specific.add.QTN[[i]])
-      
-      Add.specific.QTN.genotypic.info[[i]] <-
-        genotypes[vector.of.specific.add.QTN[[i]], ]
-      
+      vector_of_specific_add_QTN[[i]] <-
+        sample(snps, specific_QTN_number[i], replace = FALSE)
+      snps <- setdiff(snps, vector_of_specific_add_QTN[[i]])
+      add_specific_QTN_genotypic_info[[i]] <-
+        genotypes[vector_of_specific_add_QTN[[i]], ]
       data.table::fwrite(
-        Add.specific.QTN.genotypic.info[[i]],
+        add_specific_QTN_genotypic_info[[i]],
         paste0(
           "Trait",
           i,
-          ".Additive.Genotypic.info.for.",
-          specific.QTN.number[i],
-          ".specific.QTN",
+          "_Additive_Genotypic_info_for_",
+          specific_QTN_number[i],
+          "_specific_QTN",
           ".txt"
         ),
         row.names = FALSE,
@@ -82,15 +75,14 @@ QTN_partially_pleiotropic <-
         na = NA
       )
     }
-    
     # Create an output file that gives the chromosome, bp, and
     # additive effect of the effect sizes, as well as the seed
     write.table(
       c(seed, ss),
       paste0(
-        "seed.number.for.",
-        paste0(specific.QTN.number + overlap, collapse = "_"),
-        "Add.QTN",
+        "seed_number_for_",
+        paste0(specific_QTN_number + overlap, collapse = "_"),
+        "Add_QTN",
         ".txt"
       ),
       row.names = FALSE,
@@ -98,28 +90,24 @@ QTN_partially_pleiotropic <-
       sep = "\t",
       quote = FALSE
     )
-    
-    if (!is.null(overlapE) | !is.null(specific.E.QTN.number)) {
-      # Randomly select (without replacement) 2*k epistatic QTN, 
+    if (!is.null(overlap_e) | !is.null(specific_e_QTN_number)) {
+      # Randomly select (without replacement) 2*k epistatic QTN,
       # and assign an effect size
       if (!is.null(seed)) {
         set.seed(seed + seed)
       }
-      
-      vector.of.pleiotropic.epi.QTN <-
-        sample(6:nrow(genotypes), (2 * overlapE), replace = FALSE)
-      Epi.pleiotropic.QTN.genotypic.info <-
-        genotypes[vector.of.pleiotropic.epi.QTN, ]
-      
-      # Create an output file that gives the chromosome, bp, and 
+      vector_of_pleiotropic_epi_QTN <-
+        sample(6:nrow(genotypes), (2 * overlap_e), replace = FALSE)
+      epi_pleiotropic_QTN_genotypic_info <-
+        genotypes[vector_of_pleiotropic_epi_QTN, ]
+      # Create an output file that gives the chromosome, bp, and
       # additive effect of the effect sizes, as well as the seed
-      
       data.table::fwrite(
-        Epi.pleiotropic.QTN.genotypic.info,
+        epi_pleiotropic_QTN_genotypic_info,
         paste0(
-          "Genotypic.information.for.",
-          overlapE,
-          ".pleiotropic.Epistatic.QTN",
+          "Genotypic_information_for_",
+          overlap_e,
+          "_pleiotropic_Epistatic_QTN",
           ".txt"
         ),
         row.names = FALSE,
@@ -127,33 +115,29 @@ QTN_partially_pleiotropic <-
         quote = FALSE,
         na = NA
       )
-      
       snpse <-
-        setdiff(6:nrow(genotypes), vector.of.pleiotropic.epi.QTN)
-      vector.of.specific.epi.QTN <- list()
-      Epi.specific.QTN.genotypic.info <- list()
+        setdiff(6:nrow(genotypes), vector_of_pleiotropic_epi_QTN)
+      vector_of_specific_epi_QTN <- list()
+      epi_specific_QTN_genotypic_info <- list()
       sse <- c()
       for (i in 1:ntraits) {
         if (!is.null(seed)) {
           sse[i] <- (seed + i) + seed
-          set.seed((seed + i) + seed)
+          set.seed( (seed + i) + seed)
         }
-        vector.of.specific.epi.QTN[[i]] <-
-          sample(snps, (2 * specific.E.QTN.number[i]), replace = FALSE)
-        
-        snps <- setdiff(snps, vector.of.specific.epi.QTN[[i]])
-        
-        Epi.specific.QTN.genotypic.info[[i]] <-
-          genotypes[vector.of.specific.epi.QTN[[i]], ]
-        
+        vector_of_specific_epi_QTN[[i]] <-
+          sample(snps, (2 * specific_e_QTN_number[i]), replace = FALSE)
+        snps <- setdiff(snps, vector_of_specific_epi_QTN[[i]])
+        epi_specific_QTN_genotypic_info[[i]] <-
+          genotypes[vector_of_specific_epi_QTN[[i]], ]
         data.table::fwrite(
-          Epi.specific.QTN.genotypic.info[[i]],
+          epi_specific_QTN_genotypic_info[[i]],
           paste0(
             "Trait",
             i,
-            ".Epistatic.Genotypic.info.for.",
-            specific.E.QTN.number[i],
-            ".specific.QTN",
+            "_Epistatic_Genotypic_info_for_",
+            specific_e_QTN_number[i],
+            "_specific_QTN",
             ".txt"
           ),
           row.names = FALSE,
@@ -162,13 +146,12 @@ QTN_partially_pleiotropic <-
           na = NA
         )
       }
-      
       write.table(
         c(seed + seed, sse),
         paste0(
-          "seed.number.for.",
-          paste0(specific.E.QTN.number + overlapE, collapse = "_"),
-          "pleiotropic.Epi.QTN",
+          "seed_number_for_",
+          paste0(specific_e_QTN_number + overlap_e, collapse = "_"),
+          "pleiotropic_Epi_QTN",
           ".txt"
         ),
         row.names = FALSE,
@@ -176,70 +159,59 @@ QTN_partially_pleiotropic <-
         sep = "\t",
         quote = FALSE
       )
-      
-      # Create a 'base line' trait, which is basically just the additive effects;
+      # Create a 'base line' trait, which is basically just the additive effects
       # this is what we would see if the heritability of the simulated
       # trait were 1
-      additive.effect.trait.object <- 
-        as.data.frame(Add.pleiotropic.QTN.genotypic.info)
-      epistatic.effect.trait.object <- 
-        as.data.frame(Epi.pleiotropic.QTN.genotypic.info)
-      
+      additive_effect_trait_object <-
+        as.data.frame(add_pleiotropic_QTN_genotypic_info)
+      epistatic_effect_trait_object <-
+        as.data.frame(epi_pleiotropic_QTN_genotypic_info)
       for (i in 1:ntraits) {
-        additive.effect.trait.object <-
-          rbind(additive.effect.trait.object,
-                as.data.frame(Add.specific.QTN.genotypic.info[[i]]))
-        
-        epistatic.effect.trait.object <-
-          rbind(epistatic.effect.trait.object,
-                as.data.frame(Epi.specific.QTN.genotypic.info[[i]]))
+        additive_effect_trait_object <-
+          rbind(additive_effect_trait_object,
+                as.data.frame(add_specific_QTN_genotypic_info[[i]]))
+        epistatic_effect_trait_object <-
+          rbind(epistatic_effect_trait_object,
+                as.data.frame(epi_specific_QTN_genotypic_info[[i]]))
       }
-      
-      rownames(additive.effect.trait.object) <- 
-        paste0("Chr_", 
-               additive.effect.trait.object$chr,
+      rownames(additive_effect_trait_object) <-
+        paste0("Chr_",
+               additive_effect_trait_object$chr,
                "_",
-               additive.effect.trait.object$pos)
-      
-      rownames(epistatic.effect.trait.object) <- 
-        paste0("Chr_", 
-               epistatic.effect.trait.object$chr,
+               additive_effect_trait_object$pos)
+      rownames(epistatic_effect_trait_object) <-
+        paste0("Chr_",
+               epistatic_effect_trait_object$chr,
                "_",
-               epistatic.effect.trait.object$pos)
-      
-      additive.effect.trait.object <- 
-        list(t(additive.effect.trait.object[, -c(1:5)]))
-      epistatic.effect.trait.object <- 
-        list(t(epistatic.effect.trait.object[, -c(1:5)]))     
-
+               epistatic_effect_trait_object$pos)
+      additive_effect_trait_object <-
+        list(t(additive_effect_trait_object[, -c(1:5)]))
+      epistatic_effect_trait_object <-
+        list(t(epistatic_effect_trait_object[, -c(1:5)]))
       return(
         list(
-          additive.effect.trait.object = additive.effect.trait.object,
-          epistatic.effect.trait.object = epistatic.effect.trait.object
+          additive_effect_trait_object = additive_effect_trait_object,
+          epistatic_effect_trait_object = epistatic_effect_trait_object
         )
       )
     } else {
-      # Create a 'base line' trait, which is basically just the additive effects;
+      # Create a 'base line' trait, which is basically just the additive effects
       # this is what we would see if the heritability of the simulated
       # trait were 1
-      additive.effect.trait.object <- 
-        as.data.frame(Add.pleiotropic.QTN.genotypic.info)
-      
+      additive_effect_trait_object <-
+        as.data.frame(add_pleiotropic_QTN_genotypic_info)
       for (i in 1:ntraits) {
-        additive.effect.trait.object <-
-          rbind(additive.effect.trait.object,
-          as.data.frame(Add.specific.QTN.genotypic.info[[i]]))
+        additive_effect_trait_object <-
+          rbind(additive_effect_trait_object,
+          as.data.frame(add_specific_QTN_genotypic_info[[i]]))
       }
-      
-      rownames(additive.effect.trait.object) <- 
-        paste0("Chr_", 
-              additive.effect.trait.object$chr,
+      rownames(additive_effect_trait_object) <-
+        paste0("Chr_",
+              additive_effect_trait_object$chr,
               "_",
-              additive.effect.trait.object$pos)
-      
-      additive.effect.trait.object <- 
-        list(t(additive.effect.trait.object[, -c(1:5)]))
-
-      return(list(additive.effect.trait.object = additive.effect.trait.object))
+              additive_effect_trait_object$pos)
+      additive_effect_trait_object <-
+        list(t(additive_effect_trait_object[, -c(1:5)]))
+      return(list(additive_effect_trait_object = additive_effect_trait_object))
     }
   }
