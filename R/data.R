399a055592e796ddfbbd2f255114233ab2671abe
samuelbfernandes <samuelfernandes@agronomo.eng.br>
2019-09-24 10:36:58 -0500

    new data

--- R/data.R
+++ R/data.R
@@ -1,8 +1,9 @@
-#' Genotypic data for 282 inbred association panel using the 55K SNP array. 
+#' Genotypic data for 282 inbred association panel using the 55K SNP array 
+#' (filtered by MAF>0.4 to reduce file size). 
 #' Reference: Cook et al. 2012. Genetic architecture of maize kernel 
 #' composition in the nested association mapping and inbred association panels. 
 #' Plant Physiology 158:824-834
 #'
-#' @format A data frame with 51286 rows and 285 variables:
+#' @format A data frame with 10650 rows and 285 variables:
 #' @source \url{10.1104/pp.111.185033}
-"SNP55K_maize282"
+"SNP55K_maize282_maf04"
