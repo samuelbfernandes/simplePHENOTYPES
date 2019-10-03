de98c371b40e0ba65d1c7e396f0d7477072061b1
samuelbfernandes <samuelfernandes@agronomo.eng.br>
2019-10-03 12:19:10 -0500

    included rep_by

--- R/QTN_linkage.R
+++ R/QTN_linkage.R
@@ -6,6 +6,9 @@
 #' @param ld = NULL,
 #' @param gdsfile NULL
 #' @param constrains = list(maf_above = NULL, maf_below = NULL)
+#' @param rep = 1,
+#' @param rep_by = 'QTN',
+#' @param export_gt = FALSE
 #' @return Genotype of selected SNPs
 #' @author Samuel Fernandes
 #' Last update: Jul 22, 2019
@@ -18,13 +21,12 @@ QTN_linkage <-
            ld = NULL,
            gdsfile = NULL,
            constrains = list(maf_above = NULL,
-                             maf_below = NULL)) {
+                             maf_below = NULL),
+           rep = NULL,
+           rep_by = NULL,
+           export_gt = NULL) {
     #---------------------------------------------------------------------------
-    # Randomly select (without replacement)
-    # k additive QTN, and assign an effect size
-    if (!is.null(seed)) {
-      set.seed(seed)
-    }
+    if (rep_by != 'QTN'){rep <- 1}
     if (any(lengths(constrains)>0)) { 
       index <- constrain(genotypes = genotypes, 
                          maf_above = constrains$maf_above,
@@ -33,86 +35,107 @@ QTN_linkage <-
       # First SNP at column 6
       index <- 6:nrow(genotypes)
     }
-    vector_of_add_QTN <-
-      sample(index, additive_QTN_number, replace = FALSE)
-    genofile <- SNPRelate::snpgdsOpen(gdsfile)
-    inf <- c()
-    sup <- c()
-    x <- 1
-    for (k in 1:length(ld)){
+    sup <- vector("list", rep)
+    inf <- vector("list", rep)
+    add_QTN_genotypic_information_sup <- vector("list", rep)
+    add_QTN_genotypic_information_inf <- vector("list", rep)
+    QTN_causing_ld <- vector("list", rep)
+    results <- vector("list", rep)
+    for(z in 1:rep){
+      if (!is.null(seed)) {
+        set.seed(seed+z)
+      }
+      vector_of_add_QTN <-
+        sample(index, additive_QTN_number, replace = FALSE)
+      genofile <- SNPRelate::snpgdsOpen(gdsfile)
+      x <- 1
+      sup_temp <- c()
+      inf_temp <- c()
       for (j in vector_of_add_QTN) {
-      ldsup <- 1
-      i <- j + 1
-      while (ldsup >= ld[k]) {
-        snp1 <-
-          gdsfmt::read.gdsn(
-            gdsfmt::index.gdsn(genofile, "genotype"),
-            start = c(j, 1),
-            count = c(1, -1)
-          )
-        snp2 <-
-          gdsfmt::read.gdsn(
-            gdsfmt::index.gdsn(genofile, "genotype"),
-            start = c(i, 1),
-            count = c(1, -1)
-          )
-        ldsup <-
-          abs(SNPRelate::snpgdsLDpair(snp1, snp2, method = "composite"))
-        if (is.nan(ldsup)) {
-          SNPRelate::snpgdsClose(genofile)
-          stop("Monomorphic SNPs are not accepted", call. = F)
+        ldsup <- 1
+        i <- j + 1
+        while (ldsup >= ld) {
+          snp1 <-
+            gdsfmt::read.gdsn(
+              gdsfmt::index.gdsn(genofile, "genotype"),
+              start = c(j, 1),
+              count = c(1, -1)
+            )
+          snp2 <-
+            gdsfmt::read.gdsn(
+              gdsfmt::index.gdsn(genofile, "genotype"),
+              start = c(i, 1),
+              count = c(1, -1)
+            )
+          ldsup <-
+            abs(SNPRelate::snpgdsLDpair(snp1, snp2, method = "composite"))
+          if (is.nan(ldsup)) {
+            SNPRelate::snpgdsClose(genofile)
+            stop("Monomorphic SNPs are not accepted", call. = F)
+          }
+          i <- i + 1
         }
-        i <- i + 1
-      }
-      sup[x] <- i
-      ldinf <- 1
-      i2 <- j - 1
-      while (ldinf >= ld[k]) {
-        snp3 <-
-          gdsfmt::read.gdsn(
-            gdsfmt::index.gdsn(genofile, "genotype"),
-            start = c(i2, 1),
-            count = c(1, -1)
-          )
-        ldinf <-
-          abs(SNPRelate::snpgdsLDpair(snp1, snp3, method = "composite"))
-        if (is.nan(ldinf)) {
-          SNPRelate::snpgdsClose(genofile)
-          stop("Monomorphic SNPs are not accepted", call. = F)
+        sup_temp[x] <- i
+        ldinf <- 1
+        i2 <- j - 1
+        while (ldinf >= ld) {
+          snp3 <-
+            gdsfmt::read.gdsn(
+              gdsfmt::index.gdsn(genofile, "genotype"),
+              start = c(i2, 1),
+              count = c(1, -1)
+            )
+          ldinf <-
+            abs(SNPRelate::snpgdsLDpair(snp1, snp3, method = "composite"))
+          if (is.nan(ldinf)) {
+            SNPRelate::snpgdsClose(genofile)
+            stop("Monomorphic SNPs are not accepted", call. = F)
+          }
+          i2 <- i2 - 1
         }
-        i2 <- i2 - 1
+        inf_temp[x] <- i2
+        x <- x + 1
       }
-      inf[x] <- i2
-      x <- x + 1
-      }
+      # close the genotype file
+      SNPRelate::snpgdsClose(genofile)
+      sup[[z]] <- sup_temp
+      inf[[z]] <- inf_temp
+      QTN_causing_ld[[z]] <- 
+        data.frame(SNP = "cause_of_LD", genotypes[vector_of_add_QTN, ])
+      add_QTN_genotypic_information_sup[[z]] <- 
+        data.frame(SNP = "QTN_upstream",genotypes[sup[[z]], ])
+      add_QTN_genotypic_information_inf[[z]] <- 
+        data.frame(SNP = "QTN_downstream", genotypes[inf[[z]], ])
+      results[[z]] <- rbind(QTN_causing_ld[[z]],
+                            add_QTN_genotypic_information_sup[[z]],
+                            add_QTN_genotypic_information_inf[[z]])
     }
-    # close the genotype file
-    SNPRelate::snpgdsClose(genofile)
-    add_QTN_genotypic_information_sup <- genotypes[sup, ]
-    add_QTN_genotypic_information_inf <- genotypes[inf, ]
-    # Create an output file that gives the chromosome, bp,
-    # and additive effect of the effect sizes, as well as the seed
+    results <- do.call(rbind, results)
+    results <- 
+      data.frame(rep = rep(1:rep, each=additive_QTN_number*3),
+                 results)
+    if(!export_gt){
+      results <- results[,1:6]
+    }
     write.table(
-      ifelse(
-        !is.null(seed),
-        paste0("Here_is_the_seed_number: ", seed),
-        "set_seed not assigned"
+      c(seed + 1:rep),
+      paste0(
+        "seed_number_for_",additive_QTN_number,
+        "_Add_QTN",
+        ".txt"
       ),
-      paste0("seed_number_for_", additive_QTN_number,
-             "Add_QTN", ".txt"),
       row.names = FALSE,
       col.names = FALSE,
       sep = "\t",
       quote = FALSE
     )
     data.table::fwrite(
-      add_QTN_genotypic_information_sup,
+      results,
       paste0(
         "Genotypic_information_for_",
         additive_QTN_number,
-        "LD_",
+        "_QTN_with_LD_of_",
         paste(ld, collapse = "_"),
-        "_SUP_Additive_QTN",
         ".txt"
       ),
       row.names = FALSE,
@@ -120,46 +143,15 @@ QTN_linkage <-
       quote = FALSE,
       na = NA
     )
-    data.table::fwrite(
-      add_QTN_genotypic_information_inf,
-      paste0(
-        "Genotypic_information_for_",
-        additive_QTN_number,
-        "LD_",
-        paste(ld, collapse = "_"),
-        "_INF_Additive_QTN",
-        ".txt"
-      ),
-      row.names = FALSE,
-      sep = "\t",
-      quote = FALSE,
-      na = NA
-    )
-    # Create a 'base line' trait, which is basically just the additive effects;
-    # this is what we would see if the heritability of the simulated
-    # trait were 1
-    additive_effect_trait_object_sup <-
-      t(add_QTN_genotypic_information_sup[, -c(1:5)])
-    additive_effect_trait_object_inf <-
-      t(add_QTN_genotypic_information_inf[, -c(1:5)])
-    colnames(additive_effect_trait_object_sup) <-
-      paste0(
-        "Chr_",
-        unlist(add_QTN_genotypic_information_sup[, 3]),
-        "_",
-        unlist(add_QTN_genotypic_information_sup[, 4])
-      )
-    colnames(additive_effect_trait_object_inf) <-
-      paste0(
-        "Chr_",
-        unlist(add_QTN_genotypic_information_inf[, 3]),
-        "_",
-        unlist(add_QTN_genotypic_information_inf[, 4])
-      )
-    return(
-      list(
-        additive_effect_trait_object_sup = additive_effect_trait_object_sup,
-        additive_effect_trait_object_inf = additive_effect_trait_object_inf
-      )
-    )
+    additive_effect_trait_object <- mapply(function(x,y) {
+      rownames(x) <-
+        paste0("Chr_",  x$chr, "_", x$pos)
+      rownames(y) <-
+        paste0("Chr_",  y$chr, "_", y$pos)
+      b<- list(t(x[,-(1:6)]),t(y[,-(1:6)]))
+    },
+    x=add_QTN_genotypic_information_sup,
+    y=add_QTN_genotypic_information_inf,
+    SIMPLIFY = F)
+    return(list(additive_effect_trait_object = additive_effect_trait_object))
   }
