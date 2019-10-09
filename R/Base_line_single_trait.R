2da3996ca9e6d86995a59cc2bde3161457da4da4
samuelbfernandes <samuelfernandes@agronomo.eng.br>
2019-10-09 14:35:17 -0500

    using genetic_effect

--- R/Base_line_single_trait.R
+++ R/Base_line_single_trait.R
@@ -1,14 +1,21 @@
 #' Calculate genetic value based on QTN objects.
 #' @export
 #' @param additive_object hhh
+#' @param dominance_object hhh
 #' @param epistatic_object hhh
 #' @param additive_effect hhh
+#' @param dominance_effect = NULL,
 #' @param epistatic_effect hhh
 #' @param big_additive_QTN_effect hhh
+#' @param big_dominance_QTN_effect = null
 #' @param seed hhh
 #' @param rep = NULL,
 #' @param rep_by = NULL,
 #' @param ntraits = NULL
+#' @param add = NULL,
+#' @param dom = NULL,
+#' @param epi = NULL
+#' @param sim_method = NULL,
 #' @return A vector of Genetic values
 #' @author Alex Lipka and Samuel Fernandes
 
@@ -17,121 +24,56 @@
 #'-----------------------------Base_line_single_trait----------------------------
 base_line_single_trait <-
   function(additive_object = NULL,
+           dominance_object = NULL,
            epistatic_object = NULL,
            additive_effect = NULL,
+           dominance_effect = NULL,
            epistatic_effect = NULL,
            big_additive_QTN_effect = NULL,
+           big_dominance_QTN_effect = NULL,
            seed = NULL,
            rep = NULL,
            rep_by = "experiment",
-           ntraits = 1) {
+           ntraits = 1,
+           add = NULL,
+           dom = NULL,
+           epi = NULL,           
+           sim_method = NULL
+           ) {
     #'--------------------------------------------------------------------------
     if (rep_by != 'QTN') {
       rep <- 1
-    } else if (ntraits > 1){
-      rep <- 1
     }
-    if(ntraits > 1) {    
-    n <- nrow(additive_object)
-    rownames <- rownames(additive_object)
-    addnumber <- ncol(additive_object)
-    #results <- vector("list", rep)
-    #for(z in 1:rep){
-      additive_object <- as.data.frame(additive_object)
-      #' make base_line_trait additive_component and epistatic_component
-      additive_component <-
-        as.data.frame(matrix(0, nrow = n, ncol = 1))
-      additive_component <-
-        additive_component + (additive_object[, 1] * (big_additive_QTN_effect))
-      if (addnumber >= 2){
-        for (i in 2:addnumber) {
-          additive_component <- 
-            additive_component + (additive_object[, i] *
-                                    (additive_effect[1] ^ (i - 1)))
-        }
-      }
-      rownames(additive_component) <- rownames
-      colnames(additive_component) <- "additive_effect"
-      additive_genetic_variance <- var(additive_component)
-      if (!is.null(epistatic_object)) {
-        epistatic_object <- as.data.frame(epistatic_object)
-        epistatic_component <-
-          as.data.frame( matrix( 0, nrow = n, ncol = 1))
-        enumber <- ncol(epistatic_object) / 2
-        for (i in 0:(enumber - 1)) {
-          epistatic_component <-
-            epistatic_component + ( (epistatic_object[, ( (2 * i) + 1)] *
-                                       epistatic_object[, ( (2 * i) + 2)]) *
-                                      (epistatic_effect ^ (i + 1) ) )
-        }
-        rownames(epistatic_component) <- rownames
-        colnames(epistatic_component) <- "epistatic_effect"
-        epistatic_genetic_variance <- var(epistatic_component)
-        base_line_trait <- additive_component + epistatic_component
-        results <- list(
-          base_line = base_line_trait,
-          VA = c(additive_genetic_variance),
-          VE = c(epistatic_genetic_variance)
-        )
-      } else {
-        base_line_trait <- additive_component
-        results <- list(
-          base_line = base_line_trait,
-          VA = c(additive_genetic_variance)
-        )
-      }
-    #}
-    return(results)
+    if(ntraits > 1) {
+      results <- genetic_effect(add_object = additive_object,
+                                dom_object = dominance_object,
+                                epi_object = epistatic_object,
+                                add_effect = additive_effect,
+                                dom_effect = dominance_effect,
+                                epi_effect = epistatic_effect,
+                                big_add_QTN_effect = big_additive_QTN_effect,
+                                big_dom_QTN_effect = big_dominance_QTN_effect,
+                                sim_method = sim_method,
+                                add = add,
+                                dom = dom,
+                                epi = epi)
     } else {
-      n <- nrow(additive_object[[1]])
-      rownames <- rownames(additive_object[[1]])
-      addnumber <- ncol(additive_object[[1]])
       results <- vector("list", rep)
       for(z in 1:rep){
-        additive_object[[z]] <- as.data.frame(additive_object[[z]])
-        #' make base_line_trait additive_component and epistatic_component
-        additive_component <-
-          as.data.frame(matrix(0, nrow = n, ncol = 1))
-        additive_component <-
-          additive_component + (additive_object[[z]][, 1] * (big_additive_QTN_effect))
-        if (addnumber >= 2){
-          for (i in 2:addnumber) {
-            additive_component <- 
-              additive_component + (additive_object[[z]][, i] *
-                                      (additive_effect[1] ^ (i - 1)))
-          }
-        }
-        rownames(additive_component) <- rownames
-        colnames(additive_component) <- "additive_effect"
-        additive_genetic_variance <- var(additive_component)
-        if (!is.null(epistatic_object[[z]])) {
-          epistatic_object[[z]] <- as.data.frame(epistatic_object[[z]])
-          epistatic_component <-
-            as.data.frame( matrix( 0, nrow = n, ncol = 1))
-          enumber <- ncol(epistatic_object[[z]]) / 2
-          for (i in 0:(enumber - 1)) {
-            epistatic_component <-
-              epistatic_component + ( (epistatic_object[[z]][, ( (2 * i) + 1)] *
-                                         epistatic_object[[z]][, ( (2 * i) + 2)]) *
-                                        (epistatic_effect ^ (i + 1) ) )
-          }
-          rownames(epistatic_component) <- rownames
-          colnames(epistatic_component) <- "epistatic_effect"
-          epistatic_genetic_variance <- var(epistatic_component)
-          base_line_trait <- additive_component + epistatic_component
-          results[[z]] <- list(
-            base_line = base_line_trait,
-            VA = c(additive_genetic_variance),
-            VE = c(epistatic_genetic_variance)
-          )
-        } else {
-          base_line_trait <- additive_component
-          results[[z]] <- list(
-            base_line = base_line_trait,
-            VA = c(additive_genetic_variance)
-          )
-        }
+        results[[z]] <- 
+          genetic_effect(add_object = additive_object[[z]],
+                         dom_object = dominance_object[[z]],
+                         epi_object = epistatic_object[[z]],
+                         add_effect = additive_effect,
+                         dom_effect = dominance_effect,
+                         epi_effect = epistatic_effect,
+                         big_add_QTN_effect = big_additive_QTN_effect,
+                         big_dom_QTN_effect = big_dominance_QTN_effect,
+                         sim_method = sim_method,
+                         add = add,
+                         dom = dom,
+                         epi = epi)
       }
-      return(results)
     }
+    return(results)
   }
